c86ec1a7effc2ab7cbf2760de7b69740130ed920
Anthony Cardello <acardello@outlook.com>
2024-11-03 21:02:09 -0500

    reeee

--- src/robot_arm/robot_arm/stepper_move.py
+++ src/robot_arm/robot_arm/stepper_move.py
@@ -2,11 +2,14 @@ import RPi.GPIO as GPIO
 import time
 
 # GPIO setup
+print("ok that is doing somthing")
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)  # direction pin
 GPIO.setup(20, GPIO.OUT)  # pulse pin
+GPIO.output(21, 1)
 
 def move_motor(direction, steps):
+    print("move moter is called")
     GPIO.output(21, direction)
     for _ in range(steps):
         GPIO.output(20, GPIO.HIGH)
@@ -15,6 +18,7 @@ def move_motor(direction, steps):
         time.sleep(0.001)
 
 def main():
+    print("main is here")
     # Example usage
     move_motor(GPIO.HIGH, 100)  # Move forward
     time.sleep(1)
@@ -23,4 +27,5 @@ def main():
     GPIO.cleanup()
 
 if __name__ == '__main__':
+    print("what the sigma")
     main()
